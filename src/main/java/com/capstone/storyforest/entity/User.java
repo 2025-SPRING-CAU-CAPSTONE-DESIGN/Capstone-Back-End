37df7ba5eaed7e558acfc9a76a3429d3c20c7c75
heehoonhong <gmlgns0522@naver.com>
2025-03-26 16:47:46 +0900

    feat: add user entity and connect db

---
+++ src/main/java/com/capstone/storyforest/entity/User.java
@@ -0,0 +1,26 @@
+package com.capstone.storyforest.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Setter
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private LocalDate birthDate;
+
+    private String role;
+}

b5e18025f28d4f07f069cc1d879dd90d1c679b2d
heehoonhong <gmlgns0522@naver.com>
2025-03-26 20:57:27 +0900

    fix: 회원가입 응답 통일 및 에러 수정

--- src/main/java/com/capstone/storyforest/user/service/JoinService.java
+++ src/main/java/com/capstone/storyforest/user/service/JoinService.java
@@ -1,6 +1,8 @@
 package com.capstone.storyforest.user.service;
 
 
+import com.capstone.storyforest.global.apiPaylod.code.status.ErrorStatus;
+import com.capstone.storyforest.global.apiPaylod.exception.handler.UserHandler;
 import com.capstone.storyforest.user.dto.JoinRequestDTO;
 import com.capstone.storyforest.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +19,7 @@ public class JoinService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public void joinProcess(JoinRequestDTO joinRequestDTO){
+    public User joinProcess(JoinRequestDTO joinRequestDTO){
 
         String username=joinRequestDTO.getUsername();
         String password=joinRequestDTO.getPassword();
@@ -26,12 +28,12 @@ public class JoinService {
 
         Boolean isExist=userRepository.existsByUsername(username);
 
-        if(isExist){
-            return;
+        if (isExist) {
+            throw new UserHandler(ErrorStatus.USERNAME_ALREADY_EXISTS); // username 중복 코드로 새로 만들면 더 좋음
         }
 
-        if(!password.equals(passwordConfirm)){
-            return;
+        if (!password.equals(passwordConfirm)) {
+            throw new UserHandler(ErrorStatus.PASSWORDS_NOT_MATCH); // 비밀번호 불일치도 에러 코드 새로 만들어도 좋아
         }
 
         User user=new User();
@@ -40,7 +42,7 @@ public class JoinService {
         user.setBirthDate(birthDate);
         user.setRole("ROLE_ADMIN");
 
-        userRepository.save(user);
+        return userRepository.save(user);
     }
 
 }

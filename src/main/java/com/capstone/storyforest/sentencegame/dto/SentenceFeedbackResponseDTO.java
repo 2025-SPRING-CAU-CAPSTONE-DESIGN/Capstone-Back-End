a5c69f377bbe37eb4d77eea5ffb3e1a2497ff14c
heehoonhong <gmlgns0522@naver.com>
2025-05-29 20:17:35 +0900

    fix: 점수 대신 피드백으로 응답하도록 수정

---
+++ src/main/java/com/capstone/storyforest/sentencegame/dto/SentenceFeedbackResponseDTO.java
@@ -0,0 +1,10 @@
+package com.capstone.storyforest.sentencegame.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SentenceFeedbackResponseDTO {
+    private String feedback;
+}

be4ebf54ca7ccbeedb2970f9061d500c2c21bff5
paeng <127924700+juuuuone@users.noreply.github.com>
2025-06-05 22:10:08 +0900

    dto 수정

--- src/main/java/com/capstone/storyforest/user/dto/UserResponseDTO.java
+++ src/main/java/com/capstone/storyforest/user/dto/UserResponseDTO.java
@@ -10,12 +10,14 @@ public class UserResponseDTO {
     private String username;
     private int totalScore;
     private int level;
+    private Long id;
 
     @Builder
     public UserResponseDTO(User user){
         this.username = user.getUsername();
         this.level = user.getLevel();
         this.totalScore = user.getTotalScore();
+        this.id = user.getId();
     }
 }
 

994b7790b5c9bc58e5f258340fc2c715ad483c25
heehoonhong <gmlgns0522@naver.com>
2025-07-26 00:42:28 +0900

    feat

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-set -eux
 
 echo "--------------- 서버 배포 시작 -----------------"
 sudo docker stop storyforest-server || true
-sudo docker rm   storyforest-server || true
-sudo docker pull 905809970090.dkr.ecr.ap-northeast-2.amazonaws.com/storyforest-server:latest
-sudo docker run -d --name storyforest-server -p 8080:8080 \
-  905809970090.dkr.ecr.ap-northeast-2.amazonaws.com/storyforest-server:latest
+sudo docker rm storyforest-server || true
+sudo docker pull 905809970090.dkr.ecr.ap-northeast-2.amazonaws.com/storyforest-server/storyforest-server:latest
+sudo docker run -d --name storyforest-server -p 8080:8080 905809970090.dkr.ecr.ap-northeast-2.amazonaws.com/storyforest-server/storyforest-server:latest
 echo "--------------- 서버 배포 끝 -----------------"

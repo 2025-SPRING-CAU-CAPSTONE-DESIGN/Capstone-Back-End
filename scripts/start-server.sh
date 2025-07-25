21fd4c570c1b4cf9b073129e88f246f1caf98e68
heehoonhong <gmlgns0522@naver.com>
2025-07-26 01:40:06 +0900

    fix

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 echo "--------------- 서버 배포 시작 -----------------"
 sudo docker stop storyforest-server || true
 sudo docker rm storyforest-server || true
-sudo docker pull 905809970090.dkr.ecr.ap-northeast-2.amazonaws.com/storyforest-server/storyforest-server:latest
-sudo docker run -d --name storyforest-server -p 8080:8080 905809970090.dkr.ecr.ap-northeast-2.amazonaws.com/storyforest-server/storyforest-server:latest
+sudo docker pull 905809970090.dkr.ecr.ap-northeast-2.amazonaws.com/storyforest-server:latest
+sudo docker run -d --name storyforest-server -p 8080:8080 905809970090.dkr.ecr.ap-northeast-2.amazonaws.com/storyforest-server:latest
 echo "--------------- 서버 배포 끝 -----------------"

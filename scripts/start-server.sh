0df40ef4faee27499541342f5887f570b8ef37a3
heehoonhong <gmlgns0522@naver.com>
2025-07-25 23:38:25 +0900

    feat

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop storyforest-server || true
 docker rm storyforest-server || true
-docker pull {ECR Repository 주소}/storyforest-server:latest
-docker run -d --name storyforest-server -p 8080:8080 {ECR Repository 주소}/storyforest-server:latest
+docker pull 905809970090.dkr.ecr.ap-northeast-2.amazonaws.com/storyforest-server/storyforest-server:latest
+docker run -d --name storyforest-server -p 8080:8080 905809970090.dkr.ecr.ap-northeast-2.amazonaws.com/storyforest-server/storyforest-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
